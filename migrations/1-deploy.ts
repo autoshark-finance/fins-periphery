87846730d3f69e10a8d7e2e11f85c30265b3608e
Anon <>
2021-10-23 10:58:29 +0800

    feat: change factory contract

--- migrations/1-deploy.ts
+++ migrations/1-deploy.ts
@@ -1,15 +1,16 @@
 import hre, { ethers } from 'hardhat'
 
-const BLACKHOLE = '0x0000000000000000000000000000000000000000'
-
 export async function deploy() {
   const [owner] = await hre.ethers.getSigners()
 
   const Router = await hre.ethers.getContractFactory('FinsRouter02')
-  const router = await Router.deploy('0xB7D434eeBF091Dbf30a0082d05aCC0CC937c52CE', '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c')
+  const router = await Router.deploy(
+    '0xe759Dd4B9f99392Be64f1050a6A8018f73B53a13',
+    '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'
+  )
+  // const router = await hre.ethers.getContractAt('FinsRouter02', ROUTER)
   await router.deployed()
   console.log('FinsRouter02: ', (router as any).address)
-
 }
 
 deploy()
